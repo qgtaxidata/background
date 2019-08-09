d2b3f13b8c0dc50d802c5e859f6e00b211ba644f
huange7 <1048461654@qq.com>
2019-08-10 07:45:47 +0800

    修改热力图dao层

--- src/main/java/org/QGStudio/service/impl/ThermoDiagramServiceImpl.java
+++ src/main/java/org/QGStudio/service/impl/ThermoDiagramServiceImpl.java
@@ -1,5 +1,7 @@
 package org.QGStudio.service.impl;
 
+import ch.hsr.geohash.BoundingBox;
+import ch.hsr.geohash.GeoHash;
 import lombok.extern.log4j.Log4j2;
 import org.QGStudio.dao.LocationDao;
 import org.QGStudio.model.Location;
@@ -41,12 +43,14 @@ public class ThermoDiagramServiceImpl implements ThermoDiagramService {
         long time = System.currentTimeMillis();
         // 根据该坐标到当相邻的八块geohash块
         // 这里返回的是9块，包含它本身
-        String[] strings = GeoHashUtil.findNeighborGeohash(location);
+        GeoHash[] geoHashes = GeoHashUtil.findNeighborGeohash(location);
         List<LocationWithHeight> locations = new LinkedList<>();
 
         // 遍历geohash块，并在数据库中查找该geohash块对应的子geohash块
-        for (String s :
-                strings) {
+        for (GeoHash geoHash :
+                geoHashes) {
+
+            BoundingBox boundingBox = geoHash.getBoundingBox();
 
             // 设置时间格式为 yyyy-MM-dd HH:mm:ss ,对应数据库的GPS_TIME字段的格式
             DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -61,16 +65,38 @@ public class ThermoDiagramServiceImpl implements ThermoDiagramService {
             // 根据开始的时间查看对应的数据库表
             String table = TableUtil.getTable(startDate);
             // 将查到的数据整合到List中
-            locations.addAll(locationDao.findLocation(table,format.format(startDate),format.format(endDate),s.substring(s.lastIndexOf(" ")+1)+"%"));
+          /*  locations.addAll(locationDao.findLocation(table,format.format(startDate),format.format(endDate),s.substring(s.lastIndexOf(" ")+1)+"%"));*/
+            locations.addAll(locationDao.findLocation(table,boundingBox.getMaxLon(),boundingBox.getMinLon(),
+                    boundingBox.getMaxLat(),boundingBox.getMinLat(),startDate,endDate));
         }
         // 记录日志，用户执行查询9个geohash5 成功
+        //统计权重
+        Map<String, Integer> map = new HashMap();
+        for (LocationWithHeight locationWithHeight : locations) {
+            if (!map.containsKey(locationWithHeight.getGeohash())) {
+                map.put(locationWithHeight.getGeohash(), 1);
+            } else {
+                map.put(locationWithHeight.getGeohash(), map.get(locationWithHeight.getGeohash()) + 1);
+            }
+        }
 
-        List<Point> points = new ArrayList<>();
+//计算坐标
+        Set<String> keys = map.keySet();
+        List<Point> points = new LinkedList<>();
+        for (String k : keys) {
+            Point point = new Point();
+            point.setLat(GeoHashUtil.geohash2Location(k).getLatitude());
+            point.setLng(GeoHashUtil.geohash2Location(k).getLongitude());
+            point.setCount(map.get(k));
+            points.add(point);
+        }
+
+/*        List<Point> points = new ArrayList<>();
 
         // 遍历找到的七级geohash块，并将该块的中心点的经纬度设置为该块代表的经纬度
         for (LocationWithHeight locationWithHeight:locations) {
-/*            locationWithHeight.setLatitude(GeoHashUtil.geohash2Location(locationWithHeight.getGeohash()).getLatitude());
-            locationWithHeight.setLongitude(GeoHashUtil.geohash2Location(locationWithHeight.getGeohash()).getLongitude());*/
+*//*            locationWithHeight.setLatitude(GeoHashUtil.geohash2Location(locationWithHeight.getGeohash()).getLatitude());
+            locationWithHeight.setLongitude(GeoHashUtil.geohash2Location(locationWithHeight.getGeohash()).getLongitude());*//*
             Point point = new Point();
             point.setLng(GeoHashUtil.geohash2Location(locationWithHeight.getGeohash()).getLongitude());
             point.setLat(GeoHashUtil.geohash2Location(locationWithHeight.getGeohash()).getLatitude());
@@ -78,7 +104,7 @@ public class ThermoDiagramServiceImpl implements ThermoDiagramService {
             points.add(point);
         }
         log.info("用户成功查询所有的geohash块并返回对应的坐标点和权重,数据量为{}",points.size());
-        System.out.println(System.currentTimeMillis()-time);
+        System.out.println(System.currentTimeMillis()-time);*/
         return points;
     }
 }
