7acdd4fcb800c2343f3423e7f94f1f2ecdc50402
Addition <844741135@qq.com>
2019-08-09 14:46:57 +0800

    修复了经纬度的格式化

--- src/main/java/org/QGStudio/util/DigitUtil.java
+++ src/main/java/org/QGStudio/util/DigitUtil.java
@@ -2,6 +2,8 @@ package org.QGStudio.util;
 
 import org.QGStudio.model.Location;
 
+import java.text.DecimalFormat;
+
 /**
  * @description 将经纬度的小数点位数设定为6
  * @author < a href=" ">郭沛</ a>
@@ -14,28 +16,8 @@ public class DigitUtil {
      */
     public static Location checkLocationDigit(Location location) {
 
-        String longitude = String.valueOf(location.getLongitude());
-        String latitude = String.valueOf(location.getLatitude());
-        int length_long = longitude.split(".")[1].length();
-        int length_la = latitude.split(".")[1].length();
-        if (length_long > 6) {
-            longitude = longitude.split(".")[0] + longitude.split(".")[1].substring(0, 5);
-        }
-        else {
-            while (length_long < 6) {
-                longitude = longitude + "0";
-                length_long++;
-            }
-        }
-        if (length_la > 6) {
-            latitude = latitude.split(".")[0] + latitude.split(".")[1].substring(0, 5);
-        }
-        else {
-            while (length_la < 6) {
-                latitude = latitude + "0";
-                length_la++;
-            }
-        }
+        String longitude = new DecimalFormat("0.000000").format(location.getLongitude());
+        String latitude = new DecimalFormat("0.000000").format(location.getLatitude());
         location.setLongitude(Double.parseDouble(longitude));
         location.setLatitude(Double.parseDouble(latitude));
         return location;
