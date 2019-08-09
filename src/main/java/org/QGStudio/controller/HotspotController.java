2ee98b4f01db37fa8eaa73deeb93f500a01671b3
misterchaos <kobe524348@gmail.com>
2019-08-10 07:47:07 +0800

    改了location的导包

--- src/main/java/org/QGStudio/controller/HotspotController.java
+++ src/main/java/org/QGStudio/controller/HotspotController.java
@@ -3,18 +3,19 @@ package org.QGStudio.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.extern.log4j.Log4j2;
 import org.QGStudio.dtos.ResultBean;
-import org.QGStudio.exception.CheckException;
 import org.QGStudio.model.Location;
 import org.QGStudio.service.HotspotService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
-import java.text.ParseException;
 
 /**
+ * @author < a href=v " ">郭沛</ a>
  * @description 热点推荐
- * @author < a href=" ">郭沛</ a>
  * @date 2019-08-08 19:28
  */
 @RestController(value = "/hotspot")
@@ -27,7 +28,7 @@ public class HotspotController {
     private HotspotService hotspotService;
 
     @RequestMapping(value = "/findHotspot")
-    public ResultBean<?> findHotspot(@RequestBody Location location , HttpServletResponse httpServletResponse) throws JsonProcessingException {
+    public ResultBean<?> findHotspot(@RequestBody Location location, HttpServletResponse httpServletResponse) throws JsonProcessingException {
 
         log.info(location);
         return new ResultBean<>(hotspotService.findHotspot(location));
