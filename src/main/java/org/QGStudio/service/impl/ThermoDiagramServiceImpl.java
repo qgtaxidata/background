50364e98110257656e8fcece1905a1a8f3e6d6ed
huange7 <1048461654@qq.com>
2019-08-14 10:08:48 +0800

    未来热力图更新

--- src/main/java/org/QGStudio/service/impl/ThermoDiagramServiceImpl.java
+++ src/main/java/org/QGStudio/service/impl/ThermoDiagramServiceImpl.java
@@ -2,7 +2,10 @@ package org.QGStudio.service.impl;
 
 import ch.hsr.geohash.BoundingBox;
 import ch.hsr.geohash.GeoHash;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.log4j.Log4j2;
+import org.QGStudio.correspond.HttpClient;
 import org.QGStudio.dao.LocationDao;
 import org.QGStudio.exception.CheckException;
 import org.QGStudio.model.AreaLocation;
@@ -11,14 +14,13 @@ import org.QGStudio.model.LocationWithHeight;
 import org.QGStudio.model.Point;
 import org.QGStudio.runnable.NewFindGeoHashRunnable;
 import org.QGStudio.service.ThermoDiagramService;
-import org.QGStudio.util.GCJ02_WGS84;
-import org.QGStudio.util.GeoHashUtil;
-import org.QGStudio.util.TableUtil;
-import org.QGStudio.util.TimeUtil;
+import org.QGStudio.util.*;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.util.*;
 import java.util.concurrent.CountDownLatch;
@@ -34,11 +36,17 @@ import java.util.concurrent.TimeUnit;
 @Service
 @Log4j2
 public class ThermoDiagramServiceImpl implements ThermoDiagramService {
+
     @Autowired
     private LocationDao locationDao;
 
     @Autowired
     private ThreadPoolTaskExecutor threadPoolTaskExecutor;
+
+    @Autowired
+    private ObjectFactory<HttpClient> clientBean;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     /**
      * @param :[location]
@@ -51,7 +59,6 @@ public class ThermoDiagramServiceImpl implements ThermoDiagramService {
     public List findHeatMap(Location location) throws ParseException {
 
         GCJ02_WGS84.gcj02_To_Wgs84(location);
-        log.info("the request location is " + location);
         // 根据该坐标到当相邻的八块geohash块
         // 这里返回的是9块，包含它本身
 
@@ -59,8 +66,7 @@ public class ThermoDiagramServiceImpl implements ThermoDiagramService {
         GeoHash[] geoHashes = GeoHashUtil.findNeighborGeohash(location);
         List<LocationWithHeight> locations = new Vector();
         //计数器
-        CountDownLatch countDownLatch = new CountDownLatch(geoHashes.length);
-
+        
         // 遍历geohash块，并在数据库中查找该geohash块对应的子geohash块
         for (GeoHash geoHash :
                 geoHashes) {
@@ -71,8 +77,8 @@ public class ThermoDiagramServiceImpl implements ThermoDiagramService {
             Date startDate = TimeUtil.StrToDate(location.getTime());
             Date endDate = TimeUtil.StrToDate(location.getTime());
             // 这里设置间隔为5秒，后期考虑要不要进行修改
-            // TODO 这里设置时间间隔为5秒，建议可调整该值
-            endDate.setSeconds(startDate.getSeconds() + 15);
+
+            startDate.setSeconds(startDate.getSeconds() - 15);
 
             // 根据开始的时间查看对应的数据库表
             String table = TableUtil.getGpsdataTable(startDate);
@@ -106,8 +112,7 @@ public class ThermoDiagramServiceImpl implements ThermoDiagramService {
         Date startTime = TimeUtil.StrToDate(time);
         Date endTime = TimeUtil.StrToDate(time);
         // 这里设置间隔为5秒，后期考虑要不要进行修改
-        // TODO 这里设置时间间隔为5秒，建议可调整该值
-        endTime.setSeconds(startTime.getSeconds() + 15);
+        startTime.setSeconds(startTime.getSeconds() - 15);
 
 
         // 根据开始的时间查看对应的数据库表
@@ -120,35 +125,6 @@ public class ThermoDiagramServiceImpl implements ThermoDiagramService {
         return statisticalWeight(locations);
     }
 
-
-    private List statisticalWeight(List<LocationWithHeight> locations) {
-        // 记录日志，用户执行查询9个geohash5 成功
-        //统计权重
-        Map<String, Integer> map = new HashMap();
-        for (LocationWithHeight locationWithHeight : locations) {
-            if (!map.containsKey(locationWithHeight.getGeohash())) {
-                map.put(locationWithHeight.getGeohash(), 1);
-            } else {
-                map.put(locationWithHeight.getGeohash(), map.get(locationWithHeight.getGeohash()) + 1);
-            }
-        }
-
-        //计算坐标
-        Set<String> keys = map.keySet();
-        List<Point> points = new LinkedList<>();
-        for (String k : keys) {
-            Point point = new Point();
-            point.setLat(GeoHash.fromGeohashString(k).getPoint().getLatitude());
-            point.setLng(GeoHash.fromGeohashString(k).getPoint().getLongitude());
-            GCJ02_WGS84.wgs84_To_Gcj02(point);
-            point.setCount(map.get(k));
-            points.add(point);
-        }
-        log.info("用户查询成功！返回数据，数据大小为{}", points.size());
-        return points;
-    }
-
-
     @Override
     public List findAreaMap(int area, String time) {
         List<LocationWithHeight> locations = new LinkedList<>();
@@ -156,7 +132,7 @@ public class ThermoDiagramServiceImpl implements ThermoDiagramService {
 
         Date startTime = TimeUtil.StrToDate(time);
         Date endTIme = TimeUtil.StrToDate(time);
-        endTIme.setSeconds(endTIme.getSeconds() + 15);
+        startTime.setSeconds(startTime.getSeconds() - 15);
 
         String table = TableUtil.getGpsdataTable(startTime);
 
@@ -206,7 +182,7 @@ public class ThermoDiagramServiceImpl implements ThermoDiagramService {
 
         Date startTime = TimeUtil.StrToDate(time);
         Date endTime = TimeUtil.StrToDate(time);
-        endTime.setSeconds(endTime.getSeconds() + 15);
+        startTime.setSeconds(startTime.getSeconds() - 15);
 
         String table = TableUtil.getGpsdataTable(startTime);
 
@@ -218,5 +194,116 @@ public class ThermoDiagramServiceImpl implements ThermoDiagramService {
                 maxLocation.getLatitude(), minLocation.getLatitude(), startTime, endTime);
 
         return statisticalWeight(locations);
+    }
+
+
+
+    @Override
+    public Object findFutureMap(String nowTime, String futureTime, int area, int algorithm) throws IOException {
+
+        Date startTime = TimeUtil.StrToDate(nowTime);
+        Date endTime = TimeUtil.StrToDate(nowTime);
+        startTime.setSeconds(startTime.getSeconds() - 15);
+
+        String table = TableUtil.getGpsdataTable(startTime);
+
+        log.info("用户执行查询{}地区未来热力图", area);
+        Location minLocation = AreaLocation.MIN_MAP.get(area);
+        Location maxLocation = AreaLocation.MAX_MAP.get(area);
+        log.info("查询的地区数据为:{}", AreaLocation.MAX_MAP.get(area).toString());
+        List<LocationWithHeight> locations = locationDao.findLocation(table, maxLocation.getLongitude(), minLocation.getLongitude(),
+                maxLocation.getLatitude(), minLocation.getLatitude(), startTime, endTime);
+        Map<String,Object> heatMap = new HashMap<>();
+        heatMap.put("nowTime",nowTime);
+        heatMap.put("futureTime",futureTime);
+        heatMap.put("area",area);
+        heatMap.put("algorithm",algorithm);
+        heatMap.put("heat",statisticalGeohashWeight(locations));
+        String response = clientBean.getObject().doPostWithParam(heatMap);
+
+        if (VerifyUtil.isEmpty(response)){
+            throw new CheckException("网络通讯异常，请稍后重试!");
+        }
+
+        JavaType javaType = objectMapper.getTypeFactory().constructCollectionType(List.class, LocationWithHeight.class);
+        List<LocationWithHeight> list = objectMapper.readValue(response,javaType);
+        List<Point> points = new ArrayList<>();
+        for (LocationWithHeight location:
+             list) {
+            Point point = new Point();
+            point.setLat(GeoHash.fromGeohashString(location.getGeohash()).getPoint().getLatitude());
+            point.setLng(GeoHash.fromGeohashString(location.getGeohash()).getPoint().getLongitude());
+            point.setCount(location.getCount());
+            GCJ02_WGS84.wgs84_To_Gcj02(point);
+            points.add(point);
+        }
+        list.clear();
+        return points;
+    }
+    
+    /**
+     * @title : 统计七级块的权重      
+     * @param :[locations]
+     * @return : java.util.List
+     * @author : huange7
+     * @date : 2019-08-14 7:58
+     */
+
+    private List statisticalGeohashWeight(List<LocationWithHeight> locations){
+
+        // 统计geohash块的权重
+        Map<String ,Integer> map = new HashMap<>();
+        for(LocationWithHeight locationWithHeight : locations){
+            if( !map.containsKey(locationWithHeight.getGeohash()) ){
+                map.put(locationWithHeight.getGeohash(), 1);
+            }else{
+                map.put(locationWithHeight.getGeohash(), map.get(locationWithHeight.getGeohash()) + 1);
+            }
+        }
+        Set<String> keys = map.keySet();
+        List<LocationWithHeight> geohashes = new LinkedList<>();
+        for (String k:
+             keys) {
+            LocationWithHeight locationWithHeight = new LocationWithHeight();
+            locationWithHeight.setGeohash(k);
+            locationWithHeight.setCount(map.get(k));
+            geohashes.add(locationWithHeight);
+        }
+        log.info("用户查询成功！数据大小为{}", geohashes.size());
+        return geohashes;
+    }
+
+    /**
+     * @title : 统计七级块对应的中心点的权重      
+     * @param :[locations]
+     * @return : java.util.List
+     * @author : huange7
+     * @date : 2019-08-14 8:29
+     */
+    private List statisticalWeight(List<LocationWithHeight> locations) {
+        // 记录日志，用户执行查询9个geohash5 成功
+        //统计权重
+        Map<String, Integer> map = new HashMap();
+        for (LocationWithHeight locationWithHeight : locations) {
+            if (!map.containsKey(locationWithHeight.getGeohash())) {
+                map.put(locationWithHeight.getGeohash(), 1);
+            } else {
+                map.put(locationWithHeight.getGeohash(), map.get(locationWithHeight.getGeohash()) + 1);
+            }
+        }
+
+        //计算坐标
+        Set<String> keys = map.keySet();
+        List<Point> points = new LinkedList<>();
+        for (String k : keys) {
+            Point point = new Point();
+            point.setLat(GeoHash.fromGeohashString(k).getPoint().getLatitude());
+            point.setLng(GeoHash.fromGeohashString(k).getPoint().getLongitude());
+            GCJ02_WGS84.wgs84_To_Gcj02(point);
+            point.setCount(map.get(k));
+            points.add(point);
+        }
+        log.info("用户查询成功，数据大小为{}", points.size());
+        return points;
     }
 }
